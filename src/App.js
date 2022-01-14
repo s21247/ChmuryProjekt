1af770b66df27d6e199c9658378fa86b659d0af9
s21247 <s21247@pjwstk.edu.pl>
2022-01-14 15:25:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import {db} from "./firebase-config/firebase-config";
+import {collection,getDocs, addDoc,updateDoc,doc} from "@firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
+  const [test,setTest] = useState([]);
+  const testCollectionRef = collection(db, "test")
+
+  const createUser =async () => {
+    await addDoc(testCollectionRef,{name: newName,age: Number(newAge)});
+  }
+  const updateAge = async (id, age) => {
+    const testDoc = doc(db,"test",id)
+    const newAge = {
+      age: age+1
+    }
+    console.log(newAge);
+    await updateDoc(testDoc,newAge)
+  }
+  useEffect(() => {
+    
+    const getTest = async () => {
+      const data = await getDocs(testCollectionRef);
+      setTest(data.docs.map((doc) => ({
+        ...doc.data(), id: doc.id
+      })))
+    }
+    getTest();
+  }, [testCollectionRef])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={event => {setNewName(event.target.value)}}/>
+      <input type="number" onChange={event => {setNewAge(Number(event.target.value))}}/>
+      <button onClick={createUser}>add </button>
+      {test.map(test => {
+        return <div>
+          <h1>Name: {test.name}</h1>
+          <h1>Age: {test.age}</h1>
+          <button onClick={() => updateAge(test.id,test.age)}> Increase age</button>
+        </div>
+      })}
     </div>
   );
 }
