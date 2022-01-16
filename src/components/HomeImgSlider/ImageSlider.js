d25cb9b52fc8120ca0a8e614610fba59b4edc481
Mikolaj <nogamikolaj00@gmail.com>
2022-01-16 14:55:08 +0100

    Changed Home image slider to taking images from google store

--- src/components/HomeImgSlider/ImageSlider.js
+++ src/components/HomeImgSlider/ImageSlider.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { SliderData } from './SliderData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import useFirestore from "../components2/hooks/useFirestore";
 
-const ImageSlider = ({ slides }) => {
+const ImageSlider = () => {
     const [current, setCurrent] = useState(0);
-    const length = slides.length;
+    const { docs } = useFirestore('HomeSliderImages');
+    const length = docs.length;
 
 
     const nextSlide = () => {
@@ -17,24 +18,20 @@ const ImageSlider = ({ slides }) => {
         setCurrent(current === 0 ? length - 1 : current - 1);
     };
 
-    if (!Array.isArray(slides) || slides.length <= 0) {
-        return null;
-    }
-
     const timeout = setTimeout(nextSlide, 7000);
 
     return (
         <section className='slider'>
             <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide}/>
             <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-            {SliderData.map((slide, index) => {
+            {docs && docs.map((docs, index) => {
                 return (
                     <div
                         className={index === current ? 'slide active' : 'slide'}
                         key={index}
                     >
                         {index === current && (
-                            <img src={slide.image} alt='image' className='image'/>
+                            <img src={docs.url} alt='image' className='image'/>
                         )}
                     </div>
                 );
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ImageSlider from "../HomeImgSlider/ImageSlider";
-import {SliderData} from "../HomeImgSlider/SliderData";
 import '../styles/home.css';
 
 const Home = () => {
     return (
         <>
-            <ImageSlider slides={SliderData}/>
+            <ImageSlider/>
         </>
     );
 };
