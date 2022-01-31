98149677a14e4b1d0917180e6339bba683eafcd0
Mikolaj <mikolaj.saja@gmail.com>
2022-01-31 02:40:03 +0100

    anonimus user cart

--- src/components/Hooks/emptyLocalStorage.js
+++ src/components/Hooks/emptyLocalStorage.js
@@ -1,10 +1,32 @@
 import { db } from "../../firebase-config/firebase-config";
-import {collection, addDoc} from "firebase/firestore";
+import {setDoc, doc} from "firebase/firestore";
+import {getAuth, onAuthStateChanged, signInAnonymously, signOut} from "firebase/auth";
+const auth = getAuth();
 
 async function emptyLocalStorage() {
     const arr = JSON.parse(localStorage.getItem('cart'));
 
-    await addDoc(collection(db,'cart'), Object.assign({}, arr));
+    await signInAnonymously(auth)
+        .then(() => {
+            console.log("Sign In Anonymously")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Error Code: " + errorCode);
+            console.log("Error Message: " + errorMessage);
+        });
+
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            const uid = user.uid;
+            await setDoc(doc(db,'cart', uid), Object.assign({}, arr));
+
+            signOut(auth).then();
+        }
+    })
+
+
 
     localStorage.clear()
 }
